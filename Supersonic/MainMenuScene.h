f3e931d671986c935c2042c860f87b790d07728f
John King <John@johns-air.home>
2014-04-09 19:59:33 -0400

    Added the main menu scene files

---
+++ Supersonic/MainMenuScene.h
@@ -0,0 +1,13 @@
+//
+//  MainMenuScene.h
+//  Supersonic
+//
+//  Created by John King on 4/9/14.
+//  Copyright (c) 2014 John King. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface MainMenuScene : SKScene
+
+@end
