c89cf0dfbefc463c2b8e3df93f66b0064d225ea9
John King <John@johns-air.home>
2014-04-09 20:07:23 -0400

    added the sprite select scene files

---
+++ Supersonic/SpriteSelectScene.h
@@ -0,0 +1,13 @@
+//
+//  SpriteSelectScene.h
+//  Supersonic
+//
+//  Created by John King on 4/9/14.
+//  Copyright (c) 2014 John King. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface SpriteSelectScene : SKScene
+
+@end
---
+++ Supersonic/SpriteSelectScene.m
@@ -0,0 +1,13 @@
+//
+//  SpriteSelectScene.m
+//  Supersonic
+//
+//  Created by John King on 4/9/14.
+//  Copyright (c) 2014 John King. All rights reserved.
+//
+
+#import "SpriteSelectScene.h"
+
+@implementation SpriteSelectScene
+
+@end
