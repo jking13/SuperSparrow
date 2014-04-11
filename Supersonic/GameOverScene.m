2f513f21bd92b7b1af3613110a1a2a77a7e0175f
John King <John@johns-air.home>
2014-04-10 21:21:07 -0400

    Added the game over scene files

---
+++ Supersonic/GameOverScene.m
@@ -0,0 +1,13 @@
+//
+//  GameOverScene.m
+//  Supersonic
+//
+//  Created by John King on 4/10/14.
+//  Copyright (c) 2014 John King. All rights reserved.
+//
+
+#import "GameOverScene.h"
+
+@implementation GameOverScene
+
+@end
